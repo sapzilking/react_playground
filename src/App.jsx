dc0e656b3e89538ad025d989a6b9b9d6a6a333a9
sapzilking <sapzilking@gmail.com>
2024-12-01 21:39:17 +0900

    stete 훅 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
-function handleSelect(selectedButton) {
 
-  console.log(selectedButton);
-}
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -34,7 +39,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
         </section>
       </main>
     </div>
