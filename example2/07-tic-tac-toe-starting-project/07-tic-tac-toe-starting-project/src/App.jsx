a8acde500baab6198d23ad6167549b7c18d19a27
sapzilking <sapzilking@gmail.com>
2025-01-11 16:20:01 +0900

    중복코드 제거

--- example2/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ example2/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,18 +4,23 @@ import Player from './components/Player.jsx'
 import GameBoard from './components/GampBoard.jsx'
 import Log from './components/Log.jsx'
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      let currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -36,7 +41,7 @@ function App() {
         activePlayerSymbol={activePlayer}
         turns={gameTurns} />
     </div>
-    <Log />
+    <Log turns={gameTurns} />
   </main>
 }
 
